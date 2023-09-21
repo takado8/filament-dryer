afa7e0155063c7d41bb55d68252940712f3bfa90
takado <kornelgut@gmail.com>
2023-09-22 00:11:34 +0200

    adjustable reversed mode time

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ from dht11 import get_temp_and_humidity, get_dew_point
 from thingspeak import send_data
 
 
-COLD_MODE_DURATION = 60 * 10
+COLD_MODE_DURATION = 60 * 30
+REVERSED_MODE_DURATION = 60 * 4
 AVERAGE_LEN = 10
 SLEEP_TIME = 6
 OFF = 'OFF'
 ON = 'ON'
 REVERSED = 'REVERSED'
-REVERSED_MIN_TEMP = 16
-REVERSED_MAX_TEMP = 45
-NORMAL_MAX_TEMP = 40
+REVERSED_MIN_TEMP = 20
+REVERSED_MAX_TEMP = 65
+REVERSED_COOL_DOWN_TEMP_DIFF = 10
+NORMAL_MAX_TEMP = 45
 
 peltier = Peltier()
 ds = DS1820()
 fans = Fans()
 
 
+def get_time_left(mode_duration, mode_start_time):
+    return mode_duration - round(time() - mode_start_time)
+
+
 def run():
     mode = OFF
     next_mode = ON
-
+    on_cool_down = False
     mode_start_time = 0
     aggregate_temp = []
     aggregate_temp_in = []
@@ -32,8 +38,13 @@ def run():
     aggregate_dew_point = []
 
     while True:
+        if mode == ON:
+            time_left = get_time_left(COLD_MODE_DURATION, mode_start_time)
+        elif mode == REVERSED:
+            time_left = get_time_left(REVERSED_MODE_DURATION, mode_start_time)
+        else:
+            time_left = None
         temp = ds.get_temp()
-        time_left = COLD_MODE_DURATION - round(time() - mode_start_time)
         t, h = get_temp_and_humidity()
         dew_point = get_dew_point(t, h)
         aggregate_temp.append(temp)
@@ -67,10 +78,19 @@ def run():
             fans.turn_on_fan2()
             sleep(1)
             peltier.turn_on_peltier_reversed()
+            mode_start_time = time()
             mode = REVERSED
             next_mode = OFF
+        elif mode == REVERSED and time_left > 0:
+            print(f'time left: {time_left}')
+            if temp > REVERSED_MAX_TEMP and not on_cool_down:
+                peltier.turn_off_peltier()
+                on_cool_down = True
+            elif on_cool_down and temp <= REVERSED_MAX_TEMP - REVERSED_COOL_DOWN_TEMP_DIFF:
+                on_cool_down = False
+                peltier.turn_on_peltier_reversed()
 
-        elif mode == REVERSED and temp >= REVERSED_MAX_TEMP:
+        elif mode == REVERSED:
             print('Switching to neutral state 2')
             peltier.turn_off_peltier()
             mode = OFF
